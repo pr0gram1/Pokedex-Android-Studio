b508854084f5fedaddf98491aa92ff1c4d33de28
Tomislav <39119417+pr0gram1@users.noreply.github.com>
2021-08-13 05:55:03 -0700

    Update MainActivity.java

--- Pokedex/app/src/main/java/com/example/pokedex/MainActivity.java
+++ Pokedex/app/src/main/java/com/example/pokedex/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activi4ty_main);
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         listPoker = new AdapterPokeList(this);
